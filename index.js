02df6dfaeb50d3fc121a04da02aa2e13741b1a0c
MAHIR-DEVES <mdmeherab657@gmail.com>
2025-05-23 22:58:55 +0600

    all unused comment remove

--- index.js
+++ index.js
@@ -21,9 +21,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-
     const postsCollections = client.db('RoomMatchDB').collection('posts');
 
     app.post('/posts', async (req, res) => {
@@ -128,14 +125,11 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
-    // await client.db('admin').command({ ping: 1 });
-    // console.log(
-    //   'Pinged your deployment. You successfully connected to MongoDB!'
-    // );
+    await client.db('admin').command({ ping: 1 });
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
   }
 }
 run().catch(console.dir);
